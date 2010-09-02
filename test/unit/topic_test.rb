3aa4f3331dafc343a5868f7658d5989ed4ed510b
Doug Alcorn <dougalcorn@gmail.com>
2010-09-02 17:05:05 -0400

    does the topic have a vote tally?

--- test/unit/topic_test.rb
+++ test/unit/topic_test.rb
@@ -2,4 +2,22 @@ require 'test_helper'
 
 class TopicTest < ActiveSupport::TestCase
   should have_many(:votes).dependent(:delete_all)
+
+  context "a topic" do
+    setup do
+      @topic = Factory(:topic)
+    end
+    should "have a zero tally" do
+      assert_equal 0, @topic.tally
+    end
+    context "with a vote" do
+      setup do
+        @topic.votes.create
+        @topic.reload
+      end
+      should "have a tally of 1" do
+        assert_equal 1, @topic.tally
+      end
+    end
+  end
 end
