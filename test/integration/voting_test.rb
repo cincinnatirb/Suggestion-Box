2584eec236f4d8c7c21bf48e987583d96d10100b
Doug Alcorn <dougalcorn@gmail.com>
2010-09-02 16:20:39 -0400

    Adding voting action to topics controller and a vote link to the topics list (does not actually vote)

---
+++ test/integration/voting_test.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+class VotingTest < ActionDispatch::IntegrationTest
+
+  context "vising the list of suggestions with a suggestion" do
+    setup do
+      @suggestion = Factory(:topic)
+      visit topics_path
+    end
+    should "have a vote link" do
+      assert page.has_css?("a[href='#{vote_topic_path(@suggestion)}']")
+    end
+
+    context "voting for the topic" do
+      setup do
+        within(:css, "#topic_#{@suggestion.id}") do
+          click "Vote"
+        end
+      end
+      should "respond with success" do
+        assert_equal 200, page.status_code
+      end
+    end
+  end
+end
