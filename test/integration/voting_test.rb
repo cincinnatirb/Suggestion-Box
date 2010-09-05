516eb29220f17f60f53e7e76e540b69fb63a4a14
Doug Alcorn <dougalcorn@gmail.com>
2010-09-05 17:43:05 -0400

    can you click the vote tally table header and sort the table?

--- test/integration/voting_test.rb
+++ test/integration/voting_test.rb
@@ -31,5 +31,25 @@ class VotingTest < ActionDispatch::IntegrationTest
         assert page.has_css?("#topic_#{@suggestion.id} .tally", :text => (@old_vote_count + 1).to_s), "No tally given"
       end
     end
+
+    context "sorting topics" do
+      setup do
+        @most = Factory(:topic)
+        5.times { @most.votes.create }
+        @least = Factory(:topic)
+        3.times { @least.votes.create }
+        @none = Factory(:topic)
+        visit topics_path
+      end
+      context "by vote talley" do
+        setup do
+          click "Vote Tally"
+        end
+        should "have the most suggested topic first" do
+          #assert page.has_css?("tr:first-child#topic_#{@most.id}"), "Topic with most votes isn't listed first"
+          assert page.has_css?("tbody tr:first-child td.tally", :text => @most.tally.to_s)
+        end
+      end
+    end
   end
 end
